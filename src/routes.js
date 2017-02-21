738db3987e9e1648f4e9ab90fafac3268db4c6b6
Kristin Greenslit <kristingreenslit@gmail.com>
2017-02-20 22:45:45 -0500

    set up initial route

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// Route object used to define a match between a URL and a component
+import { Route, IndexRoute } from 'react-router';
+import App from './components/app';
+
+export default (
+	<Route path="/" component={App} />
+);
